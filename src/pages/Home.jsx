7c344cfb1a83b011b2e6659b5387b35ff8e40c6b
MoBa2003 <Mohammadmahdisharafbayani@gmail.com>
2025-04-13 19:11:53 +0330

    some banner issue solved and repeating background has been added to place remaining parts of homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,32 @@
-import { useInView } from 'react-intersection-observer';
-import CountUp from 'react-countup';
-import { 
-  CssBaseline, 
-  Box, 
-  Typography,
-  Divider,
-  Stack,
-} from "@mui/material";
+import { useInView } from "react-intersection-observer";
+import CountUp from "react-countup";
+import { CssBaseline, Box, Typography, Divider, Stack } from "@mui/material";
 import React from "react";
 import BannerCard from "../components/home/BannerCard";
 import NavBar from "../components/home/NavbarCard";
-import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
-import PersonIcon from '@mui/icons-material/Person';
-import StarBorderIcon from '@mui/icons-material/StarBorder';
+//  import NavBar from "../components/Navbar";
+
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import PersonIcon from "@mui/icons-material/Person";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import patternImage from "../assets/imgs/home/Background_Page.png";
 
 const AnimatedCounter = ({ end, duration = 2 }) => {
   // محاسبه گام و مدت زمان بر اساس مقدار نهایی
-  let step = 10
-  if(end>100){step = 10;}
-  if(end>500){step =50;}
-  if(end>1000){step = 100;}
-  const adjustedDuration = end > 500 ? 
-    Math.min(duration, end / 500) : // برای اعداد بزرگ سرعت بیشتری دارد
-    duration;
+  let step = 10;
+  if (end > 100) {
+    step = 10;
+  }
+  if (end > 500) {
+    step = 50;
+  }
+  if (end > 1000) {
+    step = 100;
+  }
+  const adjustedDuration =
+    end > 500
+      ? Math.min(duration, end / 500) // برای اعداد بزرگ سرعت بیشتری دارد
+      : duration;
 
   return (
     <CountUp
@@ -37,6 +41,41 @@ const AnimatedCounter = ({ end, duration = 2 }) => {
   );
 };
 
+function FullWidthRepeatingBackground({
+  imageUrl,
+  children,
+  repeatDirection = "repeat-y", // یا 'repeat-x' یا 'repeat'
+}) {
+  return (
+    <Box
+      sx={{
+        position: "relative",
+        width: "100vw",
+        marginLeft: "calc(-50vw + 50%)", // تصحیح برای centering layout
+        overflow: "hidden",
+      }}
+    >
+      {/* لایه بک‌گراند */}
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundImage: `url(${imageUrl})`,
+          backgroundRepeat: repeatDirection,
+          backgroundSize: "auto",
+          zIndex: 0,
+        }}
+      />
+
+      {/* محتوای اصلی */}
+      <Box sx={{ position: "relative", zIndex: 1 }}>{children}</Box>
+    </Box>
+  );
+}
+
 export default function Home() {
   return (
     <Box>
@@ -46,7 +85,7 @@ export default function Home() {
           width: "100vw",
           padding: "0",
           height: "40rem",
-          position: "fixed",
+          position: "absolute", // تغییر از fixed به absolute
           top: 0,
           left: 0,
           zIndex: 0,
@@ -54,173 +93,254 @@ export default function Home() {
       >
         <BannerCard />
       </Box>
-      
-      <NavBar/>
-      
-      <Box sx={{
-        position: 'relative',
-        mt: {xs: '15rem', md: '15rem'},
-        ml: {xs: '55%', md: '55%',lg:'65%',xl : '75%'},
-        maxWidth: {xs: '90%', md: '30%'},
-        textAlign: {xs: 'center', md: 'left'}
-      }}>
-        <Typography 
-          variant="h3" 
+
+      <NavBar />
+
+      <Box
+        sx={{
+          position: "relative",
+          mt: { xs: "15rem", md: "15rem" },
+          ml: { xs: "55%", md: "55%", lg: "65%", xl: "75%" },
+          maxWidth: { xs: "90%", md: "30%" },
+          textAlign: { xs: "center", md: "left" },
+        }}
+      >
+        <Typography
+          variant="h3"
           sx={{
-            fontWeight: 'bold',
-            fontSize:{xs:'140%',sm:'200%',md:'200%',lg:'250%'},
+            fontWeight: "bold",
+            fontSize: { xs: "140%", sm: "200%", md: "200%", lg: "250%" },
             lineHeight: 1.2,
-            mb: 2
+            mb: 2,
           }}
         >
           وقتشه شروع کنیم!
         </Typography>
-        
-        <Typography 
+
+        <Typography
           sx={{
-            fontSize: {xs: '73%', md: '110%', lg : '140%'},
-            color: 'text.secondary',
+            fontSize: { xs: "73%", md: "110%", lg: "140%" },
+            color: "text.secondary",
             mb: 1,
             fontWeight: 500,
-            marginLeft:'-5%'
+            marginLeft: "-5%",
           }}
         >
-          عضو جیمباتو شو تا استارت کاررو بزنیم 
+          عضو جیمباتو شو تا استارت کاررو بزنیم
         </Typography>
-        
-        <Typography 
+
+        <Typography
           sx={{
-            fontSize: {xs: '73%', md: '110%', lg : '140%'},
-            color: 'text.disabled',
-            fontStyle: 'italic',
-            marginLeft:{xs:'-2%',md:'20%',lg:'20%'}
+            fontSize: { xs: "73%", md: "110%", lg: "140%" },
+            color: "text.disabled",
+            fontStyle: "italic",
+            marginLeft: { xs: "-2%", md: "20%", lg: "20%" },
           }}
         >
           تازه شروع کاره...
         </Typography>
       </Box>
-     
+
       <Box
         sx={{
-          height: '4px',
-          width: '40%',
-          ml: '30%',
+          height: "4px",
+          width: "40%",
+          ml: "30%",
           mt: 45,
-          backgroundImage: 'linear-gradient(to right, transparent, #8BC34A, #4CAF50, #8BC34A, transparent)',
-          borderRadius: '3px',
-          boxShadow: '0 2px 12px rgba(76, 175, 80, 0.4)'
+          backgroundImage:
+            "linear-gradient(to right, transparent, #8BC34A, #4CAF50, #8BC34A, transparent)",
+          borderRadius: "3px",
+          boxShadow: "0 2px 12px rgba(76, 175, 80, 0.4)",
         }}
       />
 
       {/* Stats Section */}
-      <Box sx={{ 
-        mt: '7rem', 
-        px: 2,
-        display: 'flex',
-        justifyContent: 'center'
-      }}>
-        <Stack 
-          direction={{ xs: 'column', sm: 'row' }}
+      <Box
+        sx={{
+          mt: "7rem",
+          px: 2,
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Stack
+          direction={{ xs: "column", sm: "row" }}
           spacing={{ xs: 3, sm: 3, md: 10, lg: 20, xl: 40 }}
           justifyContent="center"
           alignItems="center"
           sx={{
-            width: '100%',
+            width: "100%",
             maxWidth: 1200,
-            textAlign: 'center',
+            textAlign: "center",
           }}
         >
           {/* Item 1 - Programs */}
-          <Box sx={{ 
-            minWidth: 100,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}>
-            <Typography variant="h4" sx={{ 
-              
-              fontWeight: 'bold',
-              mb: 1,
-              fontSize: { xs: '1.5rem', sm: '2.2rem' }
-            }}>
+          <Box
+            sx={{
+              minWidth: 100,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              variant="h4"
+              sx={{
+                fontWeight: "bold",
+                mb: 1,
+                fontSize: { xs: "1.5rem", sm: "2.2rem" },
+              }}
+            >
               +<AnimatedCounter end={300} duration={5} />
             </Typography>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <Typography variant="body2" sx={{ 
-                color: 'text.secondary',
-                fontSize: { xs: '0.9rem', sm: '1rem' }
-              }}>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Typography
+                variant="body2"
+                sx={{
+                  color: "text.secondary",
+                  fontSize: { xs: "0.9rem", sm: "1rem" },
+                }}
+              >
                 برنامه نوشته شده
               </Typography>
-              <FitnessCenterIcon sx={{ 
-                color: 'success.light',
-                fontSize: '1.2rem',
-                mr: 1,
-              }}/>
+              <FitnessCenterIcon
+                sx={{
+                  color: "success.light",
+                  fontSize: "1.2rem",
+                  mr: 1,
+                }}
+              />
             </Box>
           </Box>
 
           {/* Item 2 - Users */}
-          <Box sx={{ 
-            minWidth: 100,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}>
-            <Typography variant="h4" sx={{ 
-             
-              fontWeight: 'bold',
-              mb: 1,
-              fontSize: { xs: '1.5rem', sm: '2.2rem' }
-            }}>
+          <Box
+            sx={{
+              minWidth: 100,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              variant="h4"
+              sx={{
+                fontWeight: "bold",
+                mb: 1,
+                fontSize: { xs: "1.5rem", sm: "2.2rem" },
+              }}
+            >
               +<AnimatedCounter end={1350} duration={10} />
             </Typography>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <Typography variant="body2" sx={{ 
-                color: 'text.secondary',
-                fontSize: { xs: '0.9rem', sm: '1rem' }
-              }}>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Typography
+                variant="body2"
+                sx={{
+                  color: "text.secondary",
+                  fontSize: { xs: "0.9rem", sm: "1rem" },
+                }}
+              >
                 کاربر
               </Typography>
-              <PersonIcon sx={{ 
-                color: 'info.main',
-                fontSize: '1.2rem',
-                mr: 1,
-              }}/>
+              <PersonIcon
+                sx={{
+                  color: "info.main",
+                  fontSize: "1.2rem",
+                  mr: 1,
+                }}
+              />
             </Box>
           </Box>
 
           {/* Item 3 - Coaches */}
-          <Box sx={{ 
-            minWidth: 100,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}>
-            <Typography variant="h4" sx={{ 
-             
-              fontWeight: 'bold',
-              mb: 1,
-              fontSize: { xs: '1.5rem', sm: '2.2rem' }
-            }}>
+          <Box
+            sx={{
+              minWidth: 100,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              variant="h4"
+              sx={{
+                fontWeight: "bold",
+                mb: 1,
+                fontSize: { xs: "1.5rem", sm: "2.2rem" },
+              }}
+            >
               +<AnimatedCounter end={27} duration={5} />
             </Typography>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <Typography variant="body2" sx={{ 
-                color: 'text.secondary',
-                fontSize: { xs: '0.9rem', sm: '1rem' }
-              }}>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Typography
+                variant="body2"
+                sx={{
+                  color: "text.secondary",
+                  fontSize: { xs: "0.9rem", sm: "1rem" },
+                }}
+              >
                 مربی آماده
               </Typography>
-              <StarBorderIcon sx={{ 
-                color: 'warning.main',
-                fontSize: '1.2rem',
-                mr: 1,
-              }}/>
+              <StarBorderIcon
+                sx={{
+                  color: "warning.main",
+                  fontSize: "1.2rem",
+                  mr: 1,
+                }}
+              />
             </Box>
           </Box>
         </Stack>
       </Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: { xs: "10%", sm: "10%", md: "10%" }, // تغییر margin بر اساس سایز صفحه
+        }}
+      >
+        <Typography
+          variant="h4"
+          sx={{
+            fontSize: { xs: "1.5rem", sm: "2rem", md: "2.5rem" }, // تغییر سایز فونت
+            fontWeight: "bold", // در صورت نیاز
+            textAlign: "center", // برای اطمینان از وسطچین بودن متن
+          }}
+        >
+          امکانات ویژه
+        </Typography>
+      </Box>
+
+      <Box
+        sx={{
+          height: "4px",
+          width: "10%",
+          ml: "45%",
+          mt: 3,
+          backgroundImage:
+            "linear-gradient(to right, transparent, #8BC34A, #4CAF50, #8BC34A, transparent)",
+          borderRadius: "3px",
+          boxShadow: "0 2px 12px rgba(76, 175, 80, 0.4)",
+        }}
+      />
+
+      
+
+      <FullWidthRepeatingBackground
+        imageUrl={patternImage}
+        repeatDirection="repeat-y"
+      >
+        <Box sx={{ p: 0 }}>
+         {/* Remaining content will be placed here */}
+
+
+          
+
+
+
+        </Box>
+      </FullWidthRepeatingBackground>
     </Box>
   );
 }
