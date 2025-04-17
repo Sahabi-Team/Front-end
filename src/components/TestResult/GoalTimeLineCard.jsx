a255e5845011018eeec790e904ffd0ffc63ae25b
Ali Kargar <you@example.com>
2025-04-17 14:15:00 +0330

    Revert "test resutl components and structrues are ready !"
    This reverts commit 5c635d1150ee6a04338d99011419e67927983c47.

--- src/components/TestResult/GoalTimeLineCard.jsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import { Box, Card, CardContent, Typography, Chip } from '@mui/material';
-import { styled } from '@mui/material/styles';
--- src/pages/TestResultPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Box, Container, Grid, Chip } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import BMICard from './components/BMICard';
-import BFPCard from './components/BFPCard';
-import GoalTimelineCard from './components/GoalTimelineCard';
-import CommunityCard from './components/CommunityCard';
-import UserInfoCard from './components/UserInfoCard';
