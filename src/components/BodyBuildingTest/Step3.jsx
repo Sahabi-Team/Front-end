6bbfca40253d905900e3cc4d9f0100be5397c2ad
Moein <moeinkhanmohamadi3@gmail.com>
2025-04-21 18:29:45 +0330

    some bugs of step 1-3 fixed and became more efficient

--- src/components/BodyBuildingTest/Step3.jsx
+++ src/components/BodyBuildingTest/Step3.jsx
@@ -1,28 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { Typography, Button, Grid2 } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircleRounded";
 import CircleOutlinedIcon from '@mui/icons-material/CircleOutlined';
 
 const diseases = [
   { title: "بیماری داخلی", options: ["آسم", "میگرن", "فشار خون", "سنگ کلیه", "چربی خون", "گرفتگی عروق قلب", "اخیراً جراحی شدم"] },
-  { title: "بیماری خاص", options: ["دیابت نوع ۱", "دیابت نوع ۲", "کم‌کاری تیروئید", "پرکاری تیروئید", "سکته قلبی", "سکته مغزی", "نقص عضو"] },
+  { title: "بیماری خاص", options: ["دیابت نوع 1", "دیابت نوع 2", "کم‌کاری تیروئید", "پرکاری تیروئید", "سکته قلبی", "سکته مغزی", "نقص عضو"] },
   { title: "بیماری اسکلتی", options: ["کمردرد", "آرتروز گردن", "آرتروز زانو", "دیسک گردن", "دیسک کمر", "در رفتگی شانه", "در رفتگی مچ پا"] },
   { title: "ناهنجاری اسکلتی", options: ["کف پای صاف", "خار پاشنه", "گودی کمر", "گردن رو به جلو", "زانوی پرانتزی", "زانوی ضربدری", "تنگی کانال"] },
 ];
 
-const Step3 = () => {
-  const [selectedOptions, setSelectedOptions] = useState([]);
-
+const Step3 = ({data, setData, setIsFormValid}) => {
   const handleChange = (option) => {
-    setSelectedOptions((prev) =>
-      prev.includes(option) ? prev.filter((item) => item !== option) : [...prev, option]
-    );
+    const newSelection = data.includes(option) ? data.filter((item) => item !== option) : [...data, option];
+    setData(newSelection);
   };
+
+  useEffect(() => {
+    setIsFormValid(true);
+  }, []);
+
 
   return (
     <Grid2 container spacing={3}>
       {diseases.map(({ title, options }) => (
-        <Grid2 item size={{xs: 12, sm: 6, md: 3}} key={title}>
+        <Grid2 size={{xs: 12, sm: 6, md: 3}} key={title}>
           <Typography fontSize={18} fontWeight="bold" mb={2.5}>{title}</Typography>
           {options.map((option) => (
             <Button
@@ -34,12 +36,13 @@ const Step3 = () => {
                 marginBottom: "16px",
                 padding: "10px",
                 borderRadius: "25px",
-                border: selectedOptions.includes(option) ? "1px solid #00A359" : "1px solid #ddd",
-                backgroundColor: selectedOptions.includes(option) ? "#E8F5E9" : "#FFFFFF",
+                border: data.includes(option) ? "1px solid #00A359" : "1px solid #ddd",
+                backgroundColor: data.includes(option) ? "#E8F5E9" : "#FFFFFF",
                 color: "black",
+                "&:hover": {borderColor: "#00A359"}
               }}
             >
-              {selectedOptions.includes(option) ? (<CheckCircleIcon color="primary" fontSize="small" />) : (<CircleOutlinedIcon color="action" fontSize="small" />)}
+              {data.includes(option) ? (<CheckCircleIcon color="primary" fontSize="small" />) : (<CircleOutlinedIcon color="action" fontSize="small" />)}
               <Typography variant="button" marginLeft={1}>{option}</Typography>
             </Button>
           ))}
