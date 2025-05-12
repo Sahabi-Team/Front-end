dd5827e6eca0bc86520281898e3e809a586b84e6
Ali Kargar <alikargar0901@gmail.com>
2025-05-12 16:06:33 +0330

    link added to test result page

--- src/pages/TestResultPage.jsx
+++ src/pages/TestResultPage.jsx
@@ -11,7 +11,7 @@ import Navbar from '../components/home/NavbarCard';
 import BackGround from '../assets/imgs/Subtract.png';
 import subtract from '../assets/imgs/Subtract.png';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import Footer from '../components/CompactFooter';
 
 const GreenChip = styled(Chip)(({ theme }) => ({
@@ -237,6 +237,7 @@ const TestResultPage = () => {
           <CommunityCard memberCount={memberCount} />
 
           {/* Coach List Button */}
+          <Link to="/trainers" style={{ textDecoration: 'none' }}>
           <Box sx={{ display: 'flex', justifyContent: 'center', mt: 3, mb: 5, border: 'none' }}>
             <GreenChip 
               label="لیست مربی ها" 
@@ -252,6 +253,7 @@ const TestResultPage = () => {
               }} 
             />
           </Box>
+          </Link>
         </Container>
       </Box>
       <Footer/>
