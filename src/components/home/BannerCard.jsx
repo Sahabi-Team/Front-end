ef907d3e47b419d7d8fc438ca26b57976c0198e8
MoBa2003 <Mohammadmahdisharafbayani@gmail.com>
2025-07-16 22:47:25 +0330

    bannercard_updated

--- src/components/home/BannerCard.jsx
+++ src/components/home/BannerCard.jsx
@@ -1,23 +1,37 @@
-import { Box } from '@mui/material';
 import React from 'react';
-import Banner from "../../assets/imgs/home/Banner.png";
-import Running from '../../assets/imgs/home/running_1.png'
+import { Box, useMediaQuery, useTheme } from '@mui/material';
+import Banner from '../../assets/imgs/home/Banner.png';
+import Running_ex from '../../assets/imgs/running_1 _extra_small.png';
+import Running_sm from '../../assets/imgs/running_1_small.png';
 
 export default function BannerCard() {
+  const theme = useTheme();
+
+  const isXs = useMediaQuery(theme.breakpoints.only('xs'));
+  const isSm = useMediaQuery(theme.breakpoints.only('sm'));
+
+  let imageSrc = Banner;
+  if (isXs) {
+    imageSrc = Banner;
+  } else if (isSm) {
+    imageSrc = Banner;
+  }
+
   return (
-    <Box sx={{ 
-      width: "100%",
-      height: "100%", // Takes full height from parent
-      display: "flex",
-    }}>
+    <Box
+      sx={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+      }}
+    >
       <img
-        src={Banner}
-        style={{ 
-          maxWidth: "100%",
-          maxHeight: "100%",
-          objectFit: "conver", // Ensures the full image is visible (no cropping)
-          
-        }} 
+        src={imageSrc}
+        style={{
+          maxWidth: '100%',
+          // maxHeight: '100%',
+          // objectFit: 'cover',
+        }}
         alt="Banner"
       />
     </Box>
